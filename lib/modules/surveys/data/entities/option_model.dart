6320b1f5f6c9a6aab652242dae2bee5708b33198
luispayaresj <luispj98@outlook.com>
2024-09-02 16:27:43 -0500

    Survey Module Entities

---
+++ lib/modules/surveys/data/entities/option_model.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+Option optionFromJson(String str) => Option.fromJson(json.decode(str));
+
+String optionToJson(Option data) => json.encode(data.toJson());
+
+class Option {
+    final int id;
+    final String optionText;
+    final int questionId;
+
+    Option({
+        required this.id,
+        required this.optionText,
+        required this.questionId,
+    });
+
+    factory Option.fromJson(Map<String, dynamic> json) => Option(
+        id: json["id"],
+        optionText: json["option_text"],
+        questionId: json["question_id"],
+    );
+
+    static List<Option> fromJsonList(String jsonString) {
+      Iterable l = jsonDecode(jsonString);
+      return List<Option>.from(l.map((model) => Option.fromJson(model)));
+    }
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "option_text": optionText,
+        "question_id": questionId,
+    };
+
+    @override
+  String toString() {
+    return id.toString();
+  }
+}
