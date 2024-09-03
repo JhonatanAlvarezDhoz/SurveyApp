81cfff7212d094d36726b235881c8c86d0d57e72
Juan Jose Payares <jpayares@lsv-tech.com>
2024-09-03 15:29:55 -0500

    Finshed

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:survey_app/modules/home/ui/pages/p_home.dart';
 import 'package:survey_app/modules/login/ui/pages/p_login.dart';
 import 'package:survey_app/modules/login/ui/pages/p_register.dart';
+import 'package:survey_app/modules/login/ui/pages/p_welcome.dart';
 import 'package:survey_app/routes/app_routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +15,8 @@ class AppPages {
         return CupertinoPageRoute(builder: (_) => const RegisterPage());
       case AppRoutes.login:
         return CupertinoPageRoute(builder: (_) => const LoginPage());
+      case AppRoutes.welcome:
+        return CupertinoPageRoute(builder: (_) => const WelcomePage());
 
       default:
         return CupertinoPageRoute(builder: (_) => const Scaffold());
