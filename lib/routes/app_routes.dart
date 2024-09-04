79ce04785e654590aaf343343c71638968158f18
luispayaresj <luispj98@outlook.com>
2024-09-04 09:22:07 -0500

    Questions Pages saperated from Random Survey Page

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -4,4 +4,5 @@ class AppRoutes {
   static const String login = "/login";
   static const String register = "/register";
   static const String welcome = "/welcome";
+  static const String questions = "/questionsPage";
 }
