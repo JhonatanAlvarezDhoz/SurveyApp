45712da75b67c45f06709bae457ef110cf5c711f
Juan Jose Payares <jpayares@lsv-tech.com>
2024-09-04 14:29:15 -0500

    local storage data

--- lib/modules/common/constants/storage.dart
+++ lib/modules/common/constants/storage.dart
@@ -1,4 +1,5 @@
 class Storage {
   static const String accessToken = "access_token";
-  static const String refreshToken = "refresh_token";
+  static const String username = "user_name";
+  static const String role = "role";
 }
