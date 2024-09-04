07b8eca654efd30a8938bb0301ff1949ab98de4f
Jota <jalvarez@lsv-tech.com>
2024-09-04 17:49:35 -0500

    change redirrect to Splash page if user did login

--- lib/modules/common/pages/p_splash.dart
+++ lib/modules/common/pages/p_splash.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:survey_app/modules/common/constants/storage.dart';
 import 'package:survey_app/routes/app_routes.dart';
 import 'package:survey_app/themes/theme_colors.dart';
-
+import 'package:survey_app/utils/secure_storage.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -28,10 +31,17 @@ class _SplashPageState extends State<SplashPage> {
   Future<void> validateSession() async {
     await Future.delayed(const Duration(seconds: 3), () async {
       //TODO: deleted this line with home and descomment navigate to login
+
+      final String? accessToken =
+          await SecureStorage().readSecureData(Storage.accessToken);
+
+      if (accessToken != null) {
+        Navigator.of(context).pushReplacementNamed(AppRoutes.home);
+      } else {
+        Navigator.of(context).pushReplacementNamed(AppRoutes.welcome);
+      }
+
       //Navigator.of(context).pushReplacementNamed(AppRoutes.home);
-      //Navigator.of(context).pushReplacementNamed(AppRoutes.login);
-      //Navigator.of(context).pushReplacementNamed(AppRoutes.homeMessenger);
-      Navigator.of(context).pushReplacementNamed(AppRoutes.welcome);
     });
   }
 
